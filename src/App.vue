c0a7d20e1a27d69d265bd87e11e5b4536b1e3b04
Sander <15113512583@163.com>
2022-01-07 11:15:52 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <list :listData="todoData"></list>
+  </div>
+</template>
+
+<script>
+import List from '../components/list'
+
+export default {
+  data () {
+    return {
+      todoData: [{
+        title: '待处理',
+        list: [{
+          id: 1,
+          name: '测试1'
+        }]
+      }, {
+        title: '进行中',
+        list: [{
+          id: 6,
+          name: '测试6'
+        }]
+      }, {
+        title: '已完成',
+        list: [{
+          id: 11,
+          name: '测试11'
+        }]
+      }]
+    }
+  },
+  components: {
+    List
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/list.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="list" class="clearfix">
+    <div class="list-group" v-for="(item, index) in listData" :key="index">
+      <div class="list-title">{{ item.title }}</div>
+      <draggable class="draggable" :list="item.list" group="people">
+        <transition-group>
+          <div
+            class="draggable-item"
+            v-for="element in item.list"
+            :key="element.id"
+          >
+            <list-item :objData="element"></list-item>
+          </div>
+        </transition-group>
+      </draggable>
+    </div>
+  </div>
+</template>
+
+<script>
+import ListItem from './list-item'
+import Draggable from 'vuedraggable'
+
+export default {
+  props: {
+    listData: {
+      type: [Array],
+      default: () => []
+    }
+  },
+  components: {
+    ListItem,
+    Draggable
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus">
+#list {
+  height: 100%;
+  background-color: #f5f5f5;
+  padding: 20px;
+
+  .list-group {
+    float: left;
+    width: 240px;
+    height: 100%;
+    margin-right: 20px;
+
+    .list-title {
+      font-size: 14px;
+      font-weight: 700;
+      margin-bottom: 10px;
+    }
+
+    .draggable {
+      height: 100%;
+
+      .draggable-item {
+        margin-bottom: 8px;
+      }
+    }
+  }
+}
+</style>
